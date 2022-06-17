1e6e02d3f0dcc5d75c41a9680c2138f1669e972b
tomoharuyuyama <tomoharuyuyama@gmail.com>
2022-06-17 12:54:06 +0900

    完全一致判定のプログラム

---
+++ index.py
@@ -0,0 +1,12 @@
+# 完全一致判定のプログラム
+import cv2
+import numpy as np
+
+image1 = cv2.imread('office54.png')
+image2 = cv2.imread('office54.png')
+image3 = cv2.imread('office54-eng.png')
+
+print(np.array_equal(image1, image2))
+print(np.array_equal(image1, image3))
+# True
+# False
